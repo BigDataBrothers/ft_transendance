44c48d90a5f74df84ef61b6a3a4231bbea026d36
Mehdi YASSINE <myassine@paul-f4Ar3s2.clusters.42paris.fr>
2025-01-29 19:45:12 +0100

    Refaire architech

--- srcs/django_project/urls.py
+++ srcs/django_project/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     path('auth/password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
     path("auth/", include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
-    # path('test/', TemplateView.as_view(template_name="test.html"), name="test"),
 ]
