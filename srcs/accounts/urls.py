b4bfa1e821612a37d0aba3d81dbe7fe07fb0cffd
Mehdi YASSINE <myassine@paul-f4Br1s2.clusters.42paris.fr>
2025-01-30 19:14:55 +0100

    ajout info brut dans model

--- srcs/accounts/urls.py
+++ srcs/accounts/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-
-from .views import SignUpView
+from .views import SignUpView, profile_view
 
 app_name = 'accounts'
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-    
+    path("profile/", profile_view, name="profile"),
 ]
--- srcs/accounts/views.py
+++ srcs/accounts/views.py
@@ -1,11 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
-# Create your views here.
+@login_required
+def profile_view(request):
+    try:
+        user_profile = request.user.profile
+    except Profile.DoesNotExist:
+        user_profile = Profile.objects.create(user=request.user)
+    context = {
+        'user_profile': user_profile,
+    }
+    return render(request, 'profile.html', context)
